695f024b0fd3fe0e6b2c7d0ba90df5f932cec007
Marcos Keller <marcoskeller@hotmail.com>
2022-10-25 14:42:25 -0300

    Implementacao da parte visual do projeto

---
+++ Biblioteca_Mundo_Dev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Biblioteca_Mundo_Dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
